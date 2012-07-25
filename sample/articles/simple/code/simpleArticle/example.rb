49fde5b3b880740dff8da4b00e7549fd677cbcd5
Martin Fowler <fowler@acm.org>
2012-07-24 18:33:09 -0700

    add more material to sample simple article

---
+++ sample/articles/simple/code/simpleArticle/example.rb
@@ -0,0 +1,13 @@
+class Whatever
+ # <codeFragment name = "first">
+ def example
+   look.here "some example code"
+ # <codeFragment name = "oneLine">
+   left < right
+ # </codeFragment>
+ end
+ # </codeFragment>
+ # <codeFragment name = "second">
+ some.more(example_code)
+ # </codeFragment>
+end
