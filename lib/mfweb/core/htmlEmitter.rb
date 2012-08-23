4e760b7ddbe844ed3af0f23bad29f36426120026
Martin Fowler <fowler@acm.org>
2012-08-23 12:50:27 -0400

    added clickable style to css for underline and pointer

--- lib/mfweb/core/htmlEmitter.rb
+++ lib/mfweb/core/htmlEmitter.rb
@@ -187,10 +187,6 @@ class HtmlEmitter
     attrs = {:href => uri, :rel => 'stylesheet', :type => 'text/css'}
     element_block('link', attrs){}
   end
-  def js url
-    attrs = {:src => url, :type => 'text/javascript'}
-    element_block('script', attrs)
-  end
   def markdown src
     require 'kramdown'
     @out << Kramdown::Document.new(src).to_html
